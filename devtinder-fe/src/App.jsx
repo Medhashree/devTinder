16faa010bbdb1014b2371112946af5897db6ddec
unknown <moshat.medhashree15@gmail.com>
2025-04-15 00:36:25 +0530

    Removed unnecessary files

--- devtinder-fe/src/App.jsx
+++ devtinder-fe/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import devTinderIcon from "../src/assets/devtinder-icon.png"
 import devTinderLogo from "../src/assets/devtinder-logo.png"
